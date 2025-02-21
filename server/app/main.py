9819765ea71001c06bc6664306b06b249f375c50
Guheil <xgael.sanjuan@gmail.com>
2025-02-22 04:42:23 +0800

    forgot to include the main lmao

--- server/app/main.py
+++ server/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, fetch_products
 from app.routes import auth, fetch_products, reviews
+from app.routes import auth, fetch_stores
 
 app = FastAPI()
 
@@ -24,3 +25,4 @@ def read_root():
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(fetch_products.router, prefix="/products", tags=["Products"])
 app.include_router(reviews.router, prefix="/reviews", tags=["Reviews"])
+app.include_router(fetch_stores.router, prefix="/stores", tags=["Stores"])
