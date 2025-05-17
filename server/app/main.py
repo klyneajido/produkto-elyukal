625b9f74b80b78590c025eab59684fab224512b2
Andriq Klyne Ajido <ajido.andriqklyne@gmail.com>
2025-05-17 21:57:56 +0800

    feat(api): update main application configuration

--- server/app/main.py
+++ server/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from core.cleanup import cleanup_unverified_users
+from routes import reviews
 import logging
 
 # Set up logging
@@ -28,9 +29,12 @@ scheduler.start()
 from routes import auth
 from routes import fetch_products as products
 from routes import fetch_stores as stores
+from routes import fetch_municipalities as municipalities
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(products.router, prefix="/products", tags=["products"])
 app.include_router(stores.router, prefix="/stores", tags=["stores"])
+app.include_router(reviews.router, prefix="/reviews", tags=["reviews"]) 
+app.include_router(municipalities.router, prefix="/municipalities", tags=["municipalities"])
 
 @app.on_event("shutdown")
 def shutdown_event():
